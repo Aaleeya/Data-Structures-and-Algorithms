aea76a2b7e7b51bd85f8598eb6d9ce7aad201551
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-04 15:37:02 -0500

    first commit

---
+++ src/pointers/UsingPointer.java
@@ -0,0 +1,2 @@
+package pointers;public class UsingPointer {
+}
---
+++ src/pointers/WithoutPointer.java
@@ -0,0 +1,15 @@
+package pointers;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 11;
+        int num2 = num1;
+
+        num1 = 22;
+
+        System.out.println("num 1: "+num1);
+        System.out.println("num 2: "+num2);
+    }
+}
