28ecad1a9203524aa165db57c4e2f0bc4970aa3d
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-14 18:16:34 -0500

    linkedlist code added

---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,24 @@
+package linkedList;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+}
