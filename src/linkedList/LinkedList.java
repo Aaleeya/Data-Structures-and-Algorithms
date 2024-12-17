4033875b0f21931e476ffcae1b078c40e7d6d26f
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-17 18:20:58 -0500

    linkedlist code added

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -116,4 +116,36 @@ public class LinkedList {
         }
         return false;
     }
+
+    public boolean insert(int index, int value){
+        if(index < 0 || index > length) return false;
+        if ((index == 0)){
+            prepend(value);
+            return true;
+        }
+        if(index == length){
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public void reverse(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
 }
--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(11);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
         myLinkedList.append(3);
-        myLinkedList.append(23);
-        myLinkedList.append(7);
+        myLinkedList.append(4);
+        myLinkedList.printList();
 
-        myLinkedList.set(1, 4);
+        myLinkedList.reverse();
         myLinkedList.printList();
 
 
