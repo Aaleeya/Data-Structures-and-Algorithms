c09f31376afa83a9ca475891869d84613627da9a
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-16 22:43:50 -0500

    linkedlist code added

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -4,17 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(2);
+        LinkedList myLinkedList = new LinkedList(11);
         myLinkedList.append(3);
-        //myLinkedList.printList();
+        myLinkedList.append(23);
+        myLinkedList.append(7);
 
-//        System.out.println(myLinkedList.removeLast().value);
-//        //myLinkedList.getLength();
-//        System.out.println(myLinkedList.removeLast().value);
-//        System.out.println(myLinkedList.removeLast());
-        myLinkedList.prepend(1);
-//        myLinkedList.removeFirst();
-        System.out.println(myLinkedList.get(2).value);
+        myLinkedList.set(1, 4);
+        myLinkedList.printList();
+
 
     }
 }
