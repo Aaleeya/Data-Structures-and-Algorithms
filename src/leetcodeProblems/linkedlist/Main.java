99afdac86450651156dd47b1383802fadfeecb62
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-24 13:44:24 -0500

    Leetcode linkedlist challenge added

--- src/leetcodeProblems/linkedlist/Main.java
+++ src/leetcodeProblems/linkedlist/Main.java
@@ -22,6 +22,7 @@ public class Main {
         System.out.println("1 -> 2 -> 3 -> 4 -> 5 -> 6");
         System.out.println("Middle Node: " + myLinkedList.findMiddleNode().value);
 
+
         //------------------------------------------------
         LinkedListUsingLength myLinkedList1 = new LinkedListUsingLength(1);
         myLinkedList1.append(2);
@@ -36,6 +37,21 @@ public class Main {
         System.out.println("Has Loop:");
         System.out.println( myLinkedList1.hasLoop());
 
+        //------------------------------------------------------
+        LinkedList2 myList = new LinkedList2(1);
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+
+        LinkedList2.Node result = myList.findKthFromEnd(2); // Output: Node with value 4
+        System.out.println(result.value);
+
+        result = myList.findKthFromEnd(5); // Output: Node with value 1
+        System.out.println(result.value);
+
+        result = myList.findKthFromEnd(6); // Output: null
+        System.out.println(result);
 
 
     }
