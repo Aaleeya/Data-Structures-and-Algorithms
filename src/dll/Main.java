5e2305d460eb417851a652213c184b0d2d1816b4
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-30 18:41:00 -0500

    Doubly Linked List code added

--- src/dll/Main.java
+++ src/dll/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
         myDLL.append(2);
+        myDLL.removeLast();
         myDLL.printList();
     }
 }
