68551e93b9034d6ea24da4dd38506996186e60c4
Aaleeya <shaikhaaleeya17@gmail.com>
2024-12-30 15:13:55 -0500

    Doubly Linked List code added

--- src/dll/DoublyLinkedList.java
+++ src/dll/DoublyLinkedList.java
@@ -10,6 +10,7 @@ public class DoublyLinkedList {
     class Node{
         int value;
         Node next;
+        Node prev;
 
         Node(int value){
             this.value = value;
@@ -21,5 +22,25 @@ public class DoublyLinkedList {
         head = newNode;
         tail = newNode;
         length = 1;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("Head: "+head.value);
+    }
+
+    public void getTail(){
+        System.out.println("Tail: "+tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length: "+length);
     }
 }
