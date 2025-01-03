d30047e9715a45062adc18d01b74599e9c52b2eb
Aaleeya <shaikhaaleeya17@gmail.com>
2025-01-03 17:00:37 -0500

    Doubly Linked List code added

--- src/dll/Main.java
+++ src/dll/Main.java
@@ -2,9 +2,10 @@ package dll;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList myDLL = new DoublyLinkedList(1);
-        myDLL.append(2);
-        myDLL.removeLast();
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        myDLL.append(23);
+        myDLL.prepend(11);
+       // myDLL.removeLast();
         myDLL.printList();
     }
 }
