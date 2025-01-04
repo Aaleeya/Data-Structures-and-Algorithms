94599a7490842ee5966c0a24d547ae07ca6f1880
Aaleeya <shaikhaaleeya17@gmail.com>
2025-01-03 23:06:20 -0500

    Doubly Linked List code added

--- src/dll/DoublyLinkedList.java
+++ src/dll/DoublyLinkedList.java
@@ -94,12 +94,30 @@ public class DoublyLinkedList {
             head = null;
             tail = null;
         }
-        else if(length > 1){
-            head = temp.next;
+        else{
+            head = head.next;
             head.prev = null;
             temp.next = null;
         }
         length--;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp;
+        if (index < length / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++){
+                temp = temp.next;
+            }
+        }
+        else{
+            temp = tail;
+            for (int i = length-1; i > index; i--){
+                temp = temp.prev;
+            }
+        }
         return temp;
     }
 
--- src/dll/Main.java
+++ src/dll/Main.java
@@ -2,11 +2,15 @@ package dll;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
-        myDLL.append(23);
-        myDLL.prepend(11);
-        myDLL.removeFirst();
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.prepend(0);
+        myDLL.append(3);
+       // myDLL.removeFirst();
        // myDLL.removeLast();
         myDLL.printList();
+        System.out.println(myDLL.get(1).value);
+        System.out.println(myDLL.get(3).value);
+
     }
 }
