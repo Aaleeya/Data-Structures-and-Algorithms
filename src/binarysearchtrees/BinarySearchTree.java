1a4ddaf3bca4a20b4b296afb6b599810ea4696a1
Aaleeya <shaikhaaleeya17@gmail.com>
2025-01-15 14:35:31 -0500

    Binary Search Trees code added

---
+++ src/binarysearchtrees/BinarySearchTree.java
@@ -0,0 +1,20 @@
+package binarysearchtrees;
+
+public class BinarySearchTree {
+
+    Node root;
+
+    class Node{
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
---
+++ src/binarysearchtrees/Main.java
@@ -0,0 +1,11 @@
+package binarysearchtrees;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        System.out.println("Root: " + myBST.getRoot());
+
+    }
+}
