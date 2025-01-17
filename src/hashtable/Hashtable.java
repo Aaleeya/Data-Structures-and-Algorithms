aae771d334cc103f51ef6161a3bbdb2fa4a3ad9b
Aaleeya <shaikhaaleeya17@gmail.com>
2025-01-17 18:07:28 -0500

    HashTable code added

--- src/hashtable/Hashtable.java
+++ src/hashtable/Hashtable.java
@@ -34,4 +34,14 @@ public class Hashtable {
             }
         }
     }
+
+    private int hash(String key){
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for(int i= 0; i< keyChars.length; i++){
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length; //datamap length = 7 so range = 0 to 6
+        }
+        return hash;
+    }
 }
